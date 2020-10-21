699c6fd023f1612e58d8065ecb474c35927a9d58
Zoran Terhes <zterhes1@gmail.com>
2020-10-21 17:25:30 +0200

    ikon megjelenítések

--- map.js
+++ map.js
@@ -1,18 +1,33 @@
 
-const { table } = require('table');
-const { getBorderCharacters } = require('table');
+const { table, getBorderCharacters } = require('table');
 
 const fillMap = (player, apples, width, height) => {
   const playerX = player.pos.x;
   const playerY = player.pos.y;
   const map = new Array(height);
+  const score = false;
   for (let i = 0; i < map.length; i++) {
     map[i] = new Array(width);
   }
   for (let i = 0; i < map.length; i++) {
     for (let k = 0; k < map[i].length; k++) {
+      if (i === 0 || k === 0 || i === height - 1 || k === width - 1) {
+        map[i][k] = '██';
+      }
       if (playerX === i && playerY === k) {
-        map[i][k] = 'x';
+        map[i][k] = '🙂';
+      }
+      for (let j = 0; j < apples.length; j++) {
+        if (apples[j].x === k && apples[j].y === i) {
+          if (playerX === apples[j].y && playerY === apples[j].x) {
+            player.score++;
+            map[i][k] = '🤑';
+            apples[j].y = -1;
+            apples[j].x = -1;
+          } else {
+            map[i][k] = '🍎';
+          }
+        }
       }
     }
   }
